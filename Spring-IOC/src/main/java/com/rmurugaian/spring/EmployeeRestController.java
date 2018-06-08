f093b2139a92c716a8afa0b29a5ee6d48a5fa226
Ramachandran Murugaian <rmurugaian@bridge2solutions.com>
2018-06-08 12:09:03 +0530

    ioc testing

---
+++ Spring-IOC/src/main/java/com/rmurugaian/spring/EmployeeRestController.java
@@ -0,0 +1,20 @@
+package com.rmurugaian.spring;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+/**
+ * @author rmurugaian 2018-05-22
+ */
+@RestController
+public class EmployeeRestController {
+
+    @PostMapping("/sets")
+    public Set<String> sets(@RequestParam final Set<String> names) {
+
+        return names;
+    }
+}
