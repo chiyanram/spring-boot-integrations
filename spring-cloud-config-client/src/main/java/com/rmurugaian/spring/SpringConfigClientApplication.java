20d88150e11ea2eea4145de5ced96536f2cb7a8d
Ramachandran Murugaian <ramachandran.murugaian@gmail.com>
2018-11-27 15:07:38 +0530

    config client and server changes

--- spring-cloud-config-client/src/main/java/com/rmurugaian/spring/SpringConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/com/rmurugaian/spring/SpringConfigClientApplication.java
@@ -1,7 +1,11 @@
 package com.rmurugaian.spring;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author rmurugaian 2018-05-19
@@ -11,4 +15,17 @@ public class SpringConfigClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringConfigClientApplication.class, args);
     }
+}
+
+
+@RestController
+@RefreshScope
+class ProjectNameRestController {
+
+
+    @GetMapping("/person-name")
+    public String name(@Value("${configuration.name}") final String projectName) {
+        return projectName;
+    }
+
 }
